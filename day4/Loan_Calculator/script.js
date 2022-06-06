274599682f0383ea9c1386d69892d2bf383fbdab
Anusha Raihan <you@example.com>
2022-06-05 22:31:54 -0400

    Correction to day 4 loan calculator

--- day4/Loan_Calculator/script.js
+++ day4/Loan_Calculator/script.js
@@ -10,7 +10,7 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const inBrackets = 1 + (interestInput.value/100);
-    const outBrackets = Math.power(inBrackets, yearsInput.value);
+    const outBrackets = Math.pow(inBrackets, yearsInput.value);
     const finalAmount = loanInput.value * outBrackets;
     
     output.innerHTML = finalAmount.toFixed(2);
